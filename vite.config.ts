a1f2083356528644a7dff02268fba2207da2246d
elianiva <git@elianiva.my.id>
2026-01-11 01:31:17 +0700

    chore: externalize more deps

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     target: "esnext",
     rolldownOptions: {
-      external: ["@unovis/solid"]
+      external: ["@unovis/solid", "@unovis/ts"]
     }
   },
 });
